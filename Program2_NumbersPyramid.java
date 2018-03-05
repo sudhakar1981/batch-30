c45c7cb157eca911595f2094c378f462c7117d04
kiran <kirankommana88@gmail.com>
2018-03-05 14:35:42 +0530

    my second commit

---
+++ Program2_NumbersPyramid.java
@@ -0,0 +1,17 @@
+package kiran2;
+
+public class Kiran2 
+{
+	public static void main(String[] args) 
+	{
+		
+		for (int a = 1; a < 5; a++)
+		{
+			for (int b = 0; b < a; b++)
+			{
+			System.out.print(b+1);
+			}
+		System.out.println("");
+		}
+	}
+}
